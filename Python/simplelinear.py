898f655d978ac4212009ae894403b6b8a051ce41
ヴ・タット・タン <thanh@seta-international.co.jp>
2018-05-18 14:39:18 +0900

    simple linear regression

---
+++ Python/simplelinear.py
@@ -0,0 +1,97 @@
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt
+ 
+# CSV fileを読み込み、データをロードする
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+ 
+# 文字を数字に変更する
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+ 
+# データの塊からトレーニングデータとテストデータを分ける
+def train_test_split(dataset, split):
+	train = list()
+	train_size = split * len(dataset)
+	dataset_copy = list(dataset)
+	while len(train) < train_size:
+		index = randrange(len(dataset_copy))
+		train.append(dataset_copy.pop(index))
+	return train, dataset_copy
+ 
+# 二乗平均平方根誤差を計算する
+def rmse_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		prediction_error = predicted[i] - actual[i]
+		sum_error += (prediction_error ** 2)
+	mean_error = sum_error / float(len(actual))
+	return sqrt(mean_error)
+ 
+# 評価
+def evaluate_algorithm(dataset, algorithm, split, *args):
+	train, test = train_test_split(dataset, split)
+	test_set = list()
+	for row in test:
+		row_copy = list(row)
+		row_copy[-1] = None
+		test_set.append(row_copy)
+	predicted = algorithm(train, test_set, *args)
+	actual = [row[-1] for row in test]
+	rmse = rmse_metric(actual, predicted)
+	return rmse
+ 
+# 平均値を計算する
+def mean(values):
+	return sum(values) / float(len(values))
+ 
+#  xとyの共分散を計算する
+def covariance(x, mean_x, y, mean_y):
+	covar = 0.0
+	for i in range(len(x)):
+		covar += (x[i] - mean_x) * (y[i] - mean_y)
+	return covar
+ 
+# 標準偏差を計算する
+def variance(values, mean):
+	return sum([(x-mean)**2 for x in values])
+ 
+# 係数推定
+def coefficients(dataset):
+	x = [row[0] for row in dataset]
+	y = [row[1] for row in dataset]
+	x_mean, y_mean = mean(x), mean(y)
+	b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+	b0 = y_mean - b1 * x_mean
+	return [b0, b1]
+ 
+# 最小二乗法のアルゴ
+def simple_linear_regression(train, test):
+	predictions = list()
+	b0, b1 = coefficients(train)
+	for row in test:
+		yhat = b0 + b1 * row[0]
+		predictions.append(yhat)
+	return predictions
+ 
+# データに適用してみる・実際の問題の解決
+seed(1)
+# load and prepare data
+filename = 'insurance.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+	str_column_to_float(dataset, i)
+# evaluate algorithm
+split = 0.6
+rmse = evaluate_algorithm(dataset, simple_linear_regression, split)
+print('RMSE: %.3f' % (rmse))
